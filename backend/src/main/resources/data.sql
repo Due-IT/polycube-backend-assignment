9ea6a20ff374e3e831335f5fa91684f8e4d90f89
Due-IT <yiyop@naver.com>
2026-02-24 22:44:46 +0900

    chore: data.sql 사용해 Member 더미데이터 추가

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Members
+INSERT INTO members (name, grade) VALUES ('일반인', 'NORMAL');
+INSERT INTO members (name, grade) VALUES ('우수회원', 'VIP');
+INSERT INTO members (name, grade) VALUES ('최우수회원', 'VVIP');
