6121522e2fc1b75ebcd629584904fcb3144814f0
Due-IT <yiyop@naver.com>
2026-02-25 16:03:31 +0900

    test: NormalDiscountPolicyTest 작성

---
+++ backend/src/test/java/polycube/backend/policy/NormalDiscountPolicyTest.java
@@ -0,0 +1,26 @@
+package polycube.backend.policy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import polycube.backend.model.type.Grade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NormalDiscountPolicyTest {
+
+    private final NormalDiscountPolicy policy = new NormalDiscountPolicy();
+
+    @Test
+    @DisplayName("NORMAL 등급은 0원이 할인된다")
+    void normal_grade_no_discount() {
+        // given
+        Grade grade = Grade.NORMAL;
+        int price = 10000;
+
+        // when
+        int discount = policy.calculateDiscount(grade, price);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
