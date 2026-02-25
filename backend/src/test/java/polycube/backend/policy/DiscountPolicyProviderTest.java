4b4ceb8ba264a39e75b711e672df095e60a15692
Due-IT <yiyop@naver.com>
2026-02-25 16:58:51 +0900

    test: DiscountPolicyProviderTest 작성
    DiscountPolicyProvider는 세부 등급이나 등급에따른 할인정책을 자세히 몰라도 적절한 할인정책을 제공할 수 있어야 합니다.

---
+++ backend/src/test/java/polycube/backend/policy/DiscountPolicyProviderTest.java
@@ -0,0 +1,39 @@
+package polycube.backend.policy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import polycube.backend.model.type.Grade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DiscountPolicyProviderTest {
+    private final DiscountPolicyProvider provider = new DiscountPolicyProvider(
+            new VIPDiscountPolicy(),
+            new VVIPDiscountPolicy(),
+            new NormalDiscountPolicy()
+    );
+
+    @Test
+    @DisplayName("NORMAL 등급을 넣으면 NormalDiscountPolicy 객체를 반환해야 한다")
+    void get_vip_policy() {
+        Grade grade = Grade.NORMAL;
+        DiscountPolicy policy = provider.getPolicy(grade);
+        assertThat(policy).isExactlyInstanceOf(NormalDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("VIP 등급을 넣으면 VIPDiscountPolicy 객체를 반환해야 한다")
+    void get_vip_policy() {
+        Grade grade = Grade.VIP;
+        DiscountPolicy policy = provider.getPolicy(grade);
+        assertThat(policy).isExactlyInstanceOf(VIPDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("VVIP 등급을 넣으면 VVIPDiscountPolicy 객체를 반환해야 한다")
+    void get_vvip_policy() {
+        Grade grade = Grade.VVIP;
+        DiscountPolicy policy = provider.getPolicy(grade);
+        assertThat(policy).isExactlyInstanceOf(VVIPDiscountPolicy.class);
+    }
+}
