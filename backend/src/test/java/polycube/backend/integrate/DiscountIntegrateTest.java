2cb1c92e5a2cb393be91699befd5fa50834d3af0
Due-IT <yiyop@naver.com>
2026-02-26 11:31:05 +0900

    test: 정책이 수정/삭제 되어도 과거 결제 이력은 보존되어야 한다

---
+++ backend/src/test/java/polycube/backend/integrate/DiscountIntegrateTest.java
@@ -0,0 +1,92 @@
+package polycube.backend.integrate;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import polycube.backend.fixture.MemberFixture;
+import polycube.backend.fixture.OrderFixture;
+import polycube.backend.model.entity.DiscountHistory;
+import polycube.backend.model.entity.Member;
+import polycube.backend.model.entity.Order;
+import polycube.backend.model.type.Grade;
+import polycube.backend.policy.DiscountPolicy;
+import polycube.backend.policy.VIPDiscountPolicy;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class DiscountIntegrateTest {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private DiscountHistoryRepository historyRepository;
+
+    @Autowired
+    private DiscountSettingRepository discountSettingRepository;
+
+    @Test
+    @DisplayName("할인정책이 수정되어도 과거 결제 이력은 보존되어야 한다")
+    void history_preservation_test() {
+        // given
+        DiscountPolicy policy = new VIPDiscountPolicy();
+        String policyName = policy.getPolicyName(); // VIP
+        int discountAmount = policy.getDiscountAmount(); // 1000원 할인
+        DiscountSetting setting = discountSettingRepository.findByName(policyName);
+        AppliedDiscountPolicy appliedDiscountPolicy = new AppliedDiscountPolicy(policy, setting);
+
+        Member member = MemberFixture.createMember(Grade.VIP);
+        Order order = OrderFixture.createOrder(member, 10000);
+
+        order.applyDiscounts(List.of(appliedDiscountPolicy));
+        Order savedOrder = orderRepository.save(order);
+        Long historyId = savedOrder.getDiscountHistories().getFirst().getId();
+
+        // when
+        String updatedPolicyName = "VIP 특별 할인";
+        int updatedDiscountAmount = 2000;
+        setting.update(updatedPolicyName, 0, updatedDiscountAmount);
+
+        discountSettingRepository.save(setting);
+
+        // then
+        DiscountHistory recordedHistory = historyRepository.findById(historyId).orElseThrow();
+
+        assertThat(recordedHistory.getPolicyDiscountAmount()).isEqualTo(discountAmount);
+        assertThat(recordedHistory.getAppliedDiscountAmount()).isEqualTo(discountAmount);
+        assertThat(recordedHistory.getPolicyName()).isEqualTo(policyName);
+    }
+
+    @Test
+    @DisplayName("할인정책이 삭제되어도 과거 결제 이력은 보존되어야 한다")
+    void history_preservation_test() {
+        // given
+        DiscountPolicy policy = new VIPDiscountPolicy();
+        String policyName = policy.getPolicyName(); // VIP
+        int discountAmount = policy.getDiscountAmount(); // 1000원 할인
+        DiscountSetting setting = discountSettingRepository.findByName(policyName);
+        AppliedDiscountPolicy appliedDiscountPolicy = new AppliedDiscountPolicy(policy, setting);
+
+        Member member = MemberFixture.createMember(Grade.VIP);
+        Order order = OrderFixture.createOrder(member, 10000);
+
+        order.applyDiscounts(List.of(appliedDiscountPolicy));
+        Order savedOrder = orderRepository.save(order);
+        Long historyId = savedOrder.getDiscountHistories().getFirst().getId();
+
+        // when
+        discountSettingRepository.delete(setting);
+
+        // then
+        DiscountHistory recordedHistory = historyRepository.findById(historyId).orElseThrow();
+
+        assertThat(recordedHistory.getPolicyDiscountAmount()).isEqualTo(discountAmount);
+        assertThat(recordedHistory.getAppliedDiscountAmount()).isEqualTo(discountAmount);
+        assertThat(recordedHistory.getPolicyName()).isEqualTo(policyName);
+    }
+}
